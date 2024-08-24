4bba2c6adbf498d52fa4287f5950023a336be5ee
daviidjoshua <david.joshua.galvincio.souza@ccc.ufcg.edu.br>
2024-08-24 15:29:05 -0300

    Adicionando Tipo Baralho

---
+++ src/Types/Baralho.hs
@@ -0,0 +1,29 @@
+module Types.Baralho (
+    baralho,
+    embaralhar
+) where
+
+import Types.Carta
+import Types.Elemento
+import System.Random (randomRIO)
+
+gerarCartasElemento :: Elemento -> [Carta]
+gerarCartasElemento elemento =
+    [Carta elemento valor poder "" | (valor, poder) <- zip [1..12] poderes]
+    where
+        poderes = replicate 12 Null
+
+baralho :: [Carta]
+baralho = gerarCartasElemento Fogo ++ gerarCartasElemento Agua ++ gerarCartasElemento Neve
+
+-- Embaralha uma lista usando o algoritmo de Fisher-Yates
+embaralhar :: [a] -> IO [a]
+embaralhar [] = return []
+embaralhar xs = go (length xs - 1) xs
+  where
+    go 0 list = return list
+    go n list = do
+        idx <- randomRIO (0, n)
+        let (front, x:back) = splitAt idx list
+        shuffledBack <- go (n - 1) (front ++ back)
+        return (x : shuffledBack)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,45 @@
+-- test/Spec.hs
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Types.Baralho (baralho, embaralhar)
+import System.Random (newStdGen, randomRs)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+
+-- Testes de Baralho :
+
+tests :: TestTree
+tests = testGroup "Tests" 
+    [ 
+        -- TESTES DE BARALHO
+        testCase "Test baralho length" testBaralhoLength,
+        testCase "Test embaralhar result length" testEmbaralharLength,
+        testCase "Test if baralho is shuffled" testBaralhoEmbaralhado
+    ]
+
+-- Teste para verificar se o baralho tem 36 cartas
+testBaralhoLength :: Assertion
+testBaralhoLength = do
+    let b = baralho
+    length b @?= 36
+
+-- Teste para verificar se o baralho embaralhado tem o mesmo comprimento
+testEmbaralharLength :: Assertion
+testEmbaralharLength = do
+    let b = baralho
+    bEmbaralhado <- embaralhar b
+    length b @?= length bEmbaralhado
+
+-- Teste para verificar se o baralho foi realmente embaralhado
+testBaralhoEmbaralhado :: IO ()
+testBaralhoEmbaralhado = do
+    let originalBaralho = baralho
+    embaralhado1 <- embaralhar originalBaralho
+    embaralhado2 <- embaralhar originalBaralho
+    assertBool "O baralho deveria estar embaralhado" (embaralhado1 /= originalBaralho)
+    assertBool "O baralho deveria estar embaralhado" (embaralhado2 /= originalBaralho)
+    assertBool "O baralho deveria estar embaralhado" (embaralhado1 /= embaralhado2)
+    
