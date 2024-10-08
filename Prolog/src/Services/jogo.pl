db2ef5b616c705a743a9738013497ae50afb21c8
David Joshua <128199608+daviidjoshua@users.noreply.github.com>
2024-10-07 22:54:22 -0300

    Jogo

--- Prolog/src/Services/jogo.pl
+++ Prolog/src/Services/jogo.pl
@@ -1,4 +1,8 @@
-:- module(jogo, [iniciar_jogo/3]).
+:- module(jogo, [iniciar_jogo/3,
+                 loop_jogo/1,
+                 update_elementos/3,
+                 mostrar_elementos/1,
+                 checa_vencedor/1]).
 
 :- use_module('../Util/lib.pl').
 :- use_module('../Types/elemento.pl').
@@ -6,100 +10,123 @@
 :- use_module('../Types/baralho.pl').
 :- use_module('../Types/deque.pl').
 :- use_module('batalha.pl').
-:- use_module('inimigo.pl').
 :- use_module('saidas.pl').
 
-:- dynamic estado_jogo/10.
+:- dynamic estado_jogo/8.
 
-estado_jogo(0,0, [], [], [], [], [], [], [], []).
+estado_jogo(0, 0, [], [], [], [], [], []).
 
 iniciar_jogo(Jogador, Inimigo, Vencedor) :-
     baralho:novo_baralho(TempJogador),
     baralho:novo_baralho(TempInimigo),
+
     deque:novo_deque(TempJogador, DequeJogador, BaralhoJogador),
     deque:novo_deque(TempInimigo, DequeInimigo, BaralhoInimigo),
-    
+
     assertz(estado_jogo(Jogador, Inimigo,
                         BaralhoJogador,
                         BaralhoInimigo,
                         DequeJogador,
                         DequeInimigo,
                         [[], [], []],
-                        [[], [], []],
-                        [], [])),
+                        [[], [], []])),
 
     loop_jogo(Vencedor).
 
-loop_jogo(Vencedor) :-
-    retract(estado_jogo(Jogador, Inimigo, BaralhoJogador, BaralhoInimigo, DequeJogador, DequeInimigo, ElementosJogador, ElementosInimigo, PilhaJogador, PilhaInimigo)),
+loop_jogo(Vencedor):-
+    retract(estado_jogo(Jogador, Inimigo,
+                        BaralhoJogador, BaralhoInimigo,
+                        DequeJogador, DequeInimigo,
+                        ElementosJogador, ElementosInimigo)),
     
     print_estado_jogo,
 
-    saidas:centraliza_format([bold, fg(blue)], "Escolha uma carta para jogar [1-5]:"),
     input_to_number(Index),
-    Index > 0, Index < 6,
+
+    (Index < 1; Index > 5 ->
+        writeln("Entrada inválida. Tente novamente."),
+        loop_jogo(Vencedor) 
+    ),
     
     deque:jogar_carta(Index, DequeJogador, CartaJogador, DequeJogador2),
-    deque:jogar_carta(1,DequeInimigo,CartaInimigo,DequeInimigo2),
-   % inimigo:inimigo_jogar_carta(BaralhoInimigo, CartaInimigo, BaralhoInimigo2),
-    deque:completar_deque(DequeJogador2,BaralhoJogador,NewDequeJogador,NewBaralhoJogador),
-    deque:completar_deque(DequeInimigo2,BaralhoInimigo,NewDequeInimigo,NewBaralhoInimigo),
-    CartaJogador = carta(ElementoJogador, _, PoderJogador),
-    CartaInimigo = carta(ElementoInimigo, _, PoderInimigo),
-    pega_poder_pilha(PilhaJogador, PJ),
-    pega_poder_pilha(PilhaInimigo, PI),
-    combate(CartaJogador, CartaInimigo, PJ, PI, Ganhador),
+    baralho:pegar_carta(BaralhoInimigo, CartaInimigo, newBaralhoInimigo),
+    
+    deque:completar_deque(DequeJogador2, BaralhoJogador, NewDequeJogador, NewBaralhoJogador),
+    mostrar_deque(DequeJogador3, StringDeque),
+    writeln(StringDeque),
+
+    deque:completar_deque(DequeJogador2, BaralhoJogador, NewDequeJogador, NewBaralhoJogador),
+
+    CartaJogador = carta(EleJogador,_),
+    CartaInimigo = carta(EleInimigo,_),
+    batalha:combate(CartaJogador, CartaInimigo, Ganhador),
+
+    writeln("Ganhador do combate: "), writeln(Ganhador),
+
     (Ganhador = 1 ->
-        (ElementoJogador = fogo ->
-            NovoElementosJogador = [[fogo|ElementosJogador], _, _]
-        ; ElementoJogador = agua ->
-            NovoElementosJogador = [_, [agua|ElementosJogador], _]
-        ; ElementoJogador = neve ->
-            NovoElementosJogador = [_, _, [neve|ElementosJogador]]
-        ),
-        (checa_vencedor(NovoElementosJogador) -> Vencedor is 1, 
-            saidas:centraliza("Fim do jogo, jogador venceu!")
-        ; 
-        assertz(estado_jogo(Jogador, Inimigo, NewBaralhoJogador, NewBaralhoInimigo, NewDequeJogador, NewDequeInimigo, NovoElementosJogador, ElementosInimigo, [PoderJogador|PilhaJogador], [PoderInimigo|PilhaInimigo])),
-        loop_jogo(Vencedor)
+        update_elementos(EleJogador, ElementosJogador, NewElementosJogador),
+        (checa_vencedor(NewElementosJogador) ->
+            Vencedor is 1, !
+        ;
+            assertz(estado_jogo(Jogador, Inimigo,
+                                NewBaralhoJogador, NewBaralhoInimigo,
+                                NewDequeJogador, NewDequeInimigo,
+                                NewElementosJogador, ElementosInimigo)),
+            loop_jogo(Vencedor)
         )
-
-
     ; Ganhador = 2 ->
-        (ElementoInimigo = fogo ->
-            NovoElementosInimigo = [[fogo|ElementosInimigo], _, _]
-        ; ElementoInimigo = agua ->
-            NovoElementosInimigo = [_, [agua|ElementosInimigo], _]
-        ; ElementoInimigo = neve ->
-            NovoElementosInimigo = [_, _, [neve|ElementosInimigo]]
-        ),
-        (checa_vencedor(NovoElementosInimigo) -> Vencedor is 2
-        ; 
-        assertz(estado_jogo(Jogador, Inimigo, NewBaralhoJogador, NewBaralhoInimigo, NewDequeJogador, NewDequeInimigo, ElementosJogador, NovoElementosInimigo, [PoderJogador|PilhaJogador], [PoderInimigo|PilhaInimigo])),
-        loop_jogo(Vencedor)
+        update_elementos(EleInimigo, ElementosInimigo, NewElementosInimigo),
+        (checa_vencedor(NewElementosInimigo) ->
+            Vencedor is 2, !
+        ;
+            assertz(estado_jogo(Jogador, Inimigo,
+                                NewBaralhoJogador, NewBaralhoInimigo,
+                                NewDequeJogador, NewDequeInimigo,
+                                ElementosJogador, NewElementosInimigo)),
+            loop_jogo(Vencedor)
         )
     ).
-checa_vencedor([Fogo, Agua, Neve]) :-
-    (length(Fogo, 1), length(Agua, 1), length(Neve, 1))
-    ;
-    length(Fogo, 3)
-    ;
-    length(Agua, 3)
-    ;
-    length(Neve, 3).
 
+update_elementos(Elemento, Elementos, NovoElementos):-
+    (Elemento = fogo -> NovoElementos = [[fogo|Elementos], _, _]
+    ; Elemento = agua -> NovoElementos = [_, [agua|Elementos], _]
+    ; Elemento = neve -> NovoElementos = [_, _, [neve|Elementos]]).
 
+mostrar_elementos([Fogo, Agua, Neve]):-
+    length(Fogo, QtdFogo),
+    length(Agua, QtdAgua),
+    length(Neve, QtdNeve),
+
+    write("| FOGO : "), write(QtdFogo), nl,
+    write("| ÁGUA : "), write(QtdAgua), nl,
+    write("| NEVE : "), write(QtdNeve), nl.
+
+checa_vencedor([Fogo, Agua, Neve]):-
+    length(Fogo, QtdFogo),
+    length(Agua, QtdAgua),
+    length(Neve, QtdNeve),
+
+    (QtdFogo > 0, QtdAgua > 0, QtdNeve > 0
+    ;
+    QtdFogo = 3
+    ;
+    QtdAgua = 3
+    ;
+    QtdNeve = 3).
 
 print_estado_jogo :-
     retract(estado_jogo(Jogador, Inimigo,
                         BaralhoJogador, BaralhoInimigo,
                         DequeJogador, DequeInimigo,
-                        ElementosJogador, ElementosInimigo,
-                        PilhaJogador, PilhaInimigo)),
+                        ElementosJogador, ElementosInimigo)),
     lib:clearScreen,
     exibir_placar(Jogador, Inimigo),
-    exibir_elementos(ElementoJogador, ElementoInimigo),
-    print_poderes_ativos(PilhaJogador, PilhaInimigo), nl.
+    exibir_elementos(ElementoJogador, ElementoInimigo), nl,
+
+    saidas:centraliza_format([bold, fg(red)], "Cartas do deque:\n"),
+    deque:exibir_deque(DequeJogador), nl, nl,
+
+    saidas:centraliza_format([bold, fg(blue)], "Escolha uma carta para jogar [1-5]:").
 
 exibir_elementos(ElementosJogador, ElementosInimigo) :-
     saidas:centraliza_format([bold, fg(green)], "Seus elementos acumulados:"), nl,
@@ -114,21 +141,6 @@ print_elementos_acumulados([[Fogo | _], [Agua | _], [Neve | _]]) :-
     exibir_quantidade_com_cor(fogo, Fogo),
     exibir_quantidade_com_cor(agua, Agua),
     exibir_quantidade_com_cor(neve, Neve).
-
-
-print_poderes_ativos(PoderesJogador, PoderesInimigo) :-
-    saidas:centraliza_format([bold, fg(cyan)], "Poderes Ativos do Jogador:"), nl,
-    print_lista_poderes(PoderesPlayer), nl, nl,
-    saidas:centraliza_format([bold, fg(red)], "Poderes Ativos do Inimigo:"), nl,
-    print_lista_poderes(PoderesInimigo),nl.
-
-
-print_lista_poderes([]) :-
-    saidas:centraliza("Nenhum poder ativo."), nl.
-print_lista_poderes([Poder | Resto]) :-
-    carta:mostrar_poder(Poder, NomePoder),
-    saidas:centraliza(NomePoder),
-    print_lista_poderes(Resto).
 
 exibir_quantidade_com_cor(_, 0) :- !.
 exibir_quantidade_com_cor(Elemento, Quantidade) :-
